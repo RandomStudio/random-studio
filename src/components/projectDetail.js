ae80f707622a8be4c4132fe3e6faf22743bd3000
Kangyun Wang <kevinwang8011@hotmail.com>
2019-10-15 17:46:07 +0200

    Added video controls

--- src/components/projectDetail.js
+++ src/components/projectDetail.js
@@ -1,57 +1,58 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import styles from "./projectDetail.module.scss"
 import Img from "gatsby-image"
 import ReactMarkdown from "react-markdown"
+import ProjectVideo from "./projectVideo"
 
-const ProjectDetail = ({ title, intro, content, credits }) => (
-  <div className={styles.project}>
-    <h1 className={styles.title}>{title}</h1>
-    <div className={styles.intro}>
-      <ReactMarkdown escapeHtml={false} source={intro} />
+const ProjectDetail = ({ title, intro, content, credits }) => {
+  return (
+    <div className={styles.project}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.intro}>
+        <ReactMarkdown escapeHtml={false} source={intro} />
+      </div>
+      {(content || []).map(
+        (
+          { caption, image, marginLeft, marginTop, ratio, video, width },
+          index
+        ) => (
+          <div
+            key={index}
+            className={styles.item}
+            style={{
+              "--marginTop": `${marginTop}%`,
+              "--marginLeft": `${marginLeft}%`,
+              "--width": `${width}%`,
+            }}
+          >
+            {video || image ? (
+              <>
+                {video ? (
+                  <ProjectVideo video={video} />
+                ) : image.childImageSharp ? (
+                  <Img fluid={image.childImageSharp.fluid} />
+                ) : (
+                  <img alt="" src={image} />
+                )}
+                {caption && <div className={styles.caption}>{caption}</div>}
+              </>
+            ) : (
+              <div className={styles.text}>{caption}</div>
+            )}
+          </div>
+        )
+      )}
+
+      <footer className={styles.credits}>
+        {(credits || []).map(({ key, value }) => (
+          <ul key={`${key}-${value}`} className="">
+            <li>{key}</li>
+            <li>{value}</li>
+          </ul>
+        ))}
+      </footer>
     </div>
-    {(content || []).map(
-      (
-        { caption, image, marginLeft, marginTop, ratio, video, width },
-        index
-      ) => (
-        <div
-          key={index}
-          className={styles.item}
-          style={{
-            "--marginTop": `${marginTop}%`,
-            "--marginLeft": `${marginLeft}%`,
-            "--width": `${width}%`,
-          }}
-        >
-          {video || image ? (
-            <>
-              {video ? (
-                <div style={{ paddingBottom: `${ratio}%` }}>
-                  <video src={video} loop muted autoPlay playsInline />
-                </div>
-              ) : image.childImageSharp ? (
-                <Img fluid={image.childImageSharp.fluid} />
-              ) : (
-                <img alt="" src={image} />
-              )}
-              {caption && <div className={styles.caption}>{caption}</div>}
-            </>
-          ) : (
-            <div className={styles.text}>{caption}</div>
-          )}
-        </div>
-      )
-    )}
-
-    <footer className={styles.credits}>
-      {(credits || []).map(({ key, value }) => (
-        <ul key={`${key}-${value}`} className="">
-          <li>{key}</li>
-          <li>{value}</li>
-        </ul>
-      ))}
-    </footer>
-  </div>
-)
+  )
+}
 
 export default ProjectDetail
---
+++ src/components/projectVideo.js
@@ -0,0 +1,53 @@
+import styles from "./projectVideo.module.scss"
+import React, { useRef, useState } from "react"
+
+const ProjectVideo = ({ video }) => {
+  const videoRef = useRef(null)
+
+  // TODO: Change when cms is updated
+  const [isMuted, setIsMuted] = useState(true)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  // TODO: Add hasControls from CMS
+  const hasControls = true
+
+  const handleMutedState = () => {
+    setIsMuted(prevState => !prevState)
+  }
+
+  const handlePlayingState = () => {
+    setIsPlaying(prevState => {
+      if (isPlaying) {
+        videoRef.current.pause()
+      } else {
+        videoRef.current.play()
+      }
+
+      return !prevState
+    })
+  }
+
+  return (
+    <div className={styles.videoWrapper} style={{ paddingBottom: `${56.25}%` }}>
+      <video
+        ref={videoRef}
+        src={video}
+        loop
+        muted={isMuted}
+        // autoPlay={autoplay}
+      />
+      {hasControls && (
+        <div className={styles.videoControls}>
+          <button onClick={handlePlayingState}>
+            {isPlaying ? "Pause" : "Play"}
+          </button>
+          <button onClick={handleMutedState}>
+            {isMuted ? "Unmute" : "Mute"}
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProjectVideo
