db66a73d0f42ea0cb17769446dd8ac80e470ca3c
Kangyun Wang <kevinwang8011@hotmail.com>
2019-10-18 11:41:40 +0200

    Responsive recruitee block in studio

--- src/components/Studio/Recruitee/recruitee.js
+++ src/components/Studio/Recruitee/recruitee.js
@@ -1,13 +1,29 @@
 import styles from "./recruitee.module.scss"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 const Recruitee = () => {
+  const [recruiteeData, setRecruiteeData] = useState({})
+
+  useEffect(() => {
+    fetch("https://career.recruitee.com/api/c/23038/widget/?widget=true")
+      .then(res => res.json())
+      .then(json => setRecruiteeData(json))
+      .catch(err => {
+        throw new Error(`Failed to feetch open job offers: ${err}`)
+      })
+  }, [])
+
+  if (!recruiteeData.offers || !recruiteeData.offers.length) return null
+
   return (
-    <aside>
-      Our open roles
+    <aside className={styles.recruitee}>
+      <p>Open Roles</p>
       <ul>
-        <li>Role</li>
-        <li>Role</li>
+        {recruiteeData.offers.map(offer => (
+          <li key={offer.id}>
+            <span>{offer.title}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   )
