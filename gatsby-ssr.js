e7bd0a2f95e39b87f9047ef4fcb4f80fe57c692b
Kangyun Wang <me@kyunwang.nl>
2019-10-22 16:35:17 +0200

    Add static render for helmet

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,17 @@
 // Docs: https://www.gatsbyjs.org/docs/ssr-apis/
+
+const { Helmet } = require("react-helmet")
+
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+  ])
+}
