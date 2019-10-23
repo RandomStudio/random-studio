0930653b3c9daf67ecdf18085f6c90ee80fffd21
Kangyun Wang <me@kyunwang.nl>
2019-10-23 10:07:07 +0200

    Remove test of context

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,19 +62,14 @@ const gatsbyConfig = {
   ],
 }
 
-console.log("BEFORREEEE")
+// CONTEXT is a netlify env
 if (process.env.CONTEXT === "production") {
-  console.log("CONTEXXXXT")
-
   const gaConfig = {
     resolve: `gatsby-plugin-google-analytics`,
     options: {
       trackingId: "UA-9384788-13",
-      // Defines where to place the tracking script - `true` in the head and `false` in the body
       head: true,
-      // Setting this parameter is optional
       anonymize: true,
-      // Setting this parameter is also optional
       respectDNT: true,
     },
   }
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -5,19 +5,17 @@ import styles from "./layout.module.scss"
 
 const backgroundTransitionColors = ["#0000FF", "#FFFF00", "#00FFFF", "#FF00FF"]
 
-export default ({ children }) =>
-  console.log(process.env.NODE_ENV) || (
-    <>
-      <h1>{process.env.CONTEXT}</h1>
-      <div className={styles.container}>{children}</div>
-      <div
-        className={styles.backgroundTransition}
-        style={{
-          backgroundColor:
-            backgroundTransitionColors[
-              Math.floor(Math.random() * backgroundTransitionColors.length)
-            ],
-        }}
-      />
-    </>
-  )
+export default ({ children }) => (
+  <>
+    <div className={styles.container}>{children}</div>
+    <div
+      className={styles.backgroundTransition}
+      style={{
+        backgroundColor:
+          backgroundTransitionColors[
+            Math.floor(Math.random() * backgroundTransitionColors.length)
+          ],
+      }}
+    />
+  </>
+)
