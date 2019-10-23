df178642c92a2371ab80ed94983c0cc6d15d9885
Larix Kortbeek <larixk@gmail.com>
2019-10-23 14:31:57 +0200

    Fix SEO unique key prop warning

--- src/components/Layout/seo.js
+++ src/components/Layout/seo.js
@@ -18,40 +18,6 @@ const defaultMeta = [
   { name: "msapplication-TileColor", content: "#ffffff" },
 ]
 
-const favicons = [
-  <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href="/favicons/apple-touch-icon.png"
-  />,
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="/favicons/favicon-32x32.png"
-  />,
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="16x16"
-    href="/favicons/favicon-16x16.png"
-  />,
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="192x192"
-    href="/favicons/android-chrome-192x192.png"
-  />,
-  <link rel="manifest" href="/favicons/site.webmanifest" />,
-  <link
-    rel="mask-icon"
-    href="/favicons/safari-pinned-tab.svg"
-    color="#0000ff"
-  />,
-  <link rel="manifest" href="/favicons/manifest.json" />,
-  <link rel="shortcut icon" href="/favicons/favicon.ico" />,
-]
-
 const SEO = ({ title, description, image, pathName }) => {
   const data = useStaticQuery(graphql`
     query HeaderQuery {
@@ -120,7 +86,37 @@ const SEO = ({ title, description, image, pathName }) => {
         { name: "twitter:site", content: twitterHandle },
       ]}
     >
-      {favicons}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/favicons/android-chrome-192x192.png"
+      />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#0000ff"
+      />
+      <link rel="manifest" href="/favicons/manifest.json" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
     </Helmet>
   )
 }
