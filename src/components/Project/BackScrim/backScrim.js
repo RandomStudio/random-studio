c4fd398d7f0d348103940179f611daf065c2f1a5
Kevin Wang <me@kyunwang.nl>
2019-10-23 16:31:12 +0200

    Project footer (#24)
    * Remove old css
    
    * Default no text-decoration for a tags
    
    * Setup back button with scrim for project footer
    
    * Add responsive back button with scrim - projects page
    
    * Remove unused import
    
    * Slight increase in scrim
    
    * Minor sorting
    
    * Remove unused code
    
    * Show back button on overscroll & add js disabled fallback

---
+++ src/components/Project/BackScrim/backScrim.js
@@ -0,0 +1,66 @@
+import styles from "./backScrim.module.scss"
+import React, { useEffect, useRef, useState } from "react"
+import { Link } from "gatsby"
+import { motion, AnimatePresence } from "framer-motion"
+
+const BackScrim = () => {
+  const intersectionRef = useRef()
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const handleScrimVisibility = event => {
+      if (event.deltaY > 0 && !isVisible) {
+        setIsVisible(true)
+      }
+    }
+
+    const intersectCb = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          window.addEventListener("wheel", handleScrimVisibility)
+        } else {
+          window.removeEventListener("wheel", handleScrimVisibility)
+          setIsVisible(false)
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(intersectCb)
+    observer.observe(intersectionRef.current)
+
+    return () => {
+      observer.unobserve(intersectionRef.current)
+    }
+  }, [])
+
+  return (
+    <>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            className={styles.backScrim}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Link className={styles.backButton} to="/#projects">
+              Back to projects
+            </Link>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div ref={intersectionRef} className={styles.intersectionLine} />
+
+      {/* If JS disabled - just show it */}
+      <noscript>
+        <div className={styles.backScrim}>
+          <Link className={styles.backButton} to="/#projects">
+            Back to projects
+          </Link>
+        </div>
+      </noscript>
+    </>
+  )
+}
+
+export default BackScrim
