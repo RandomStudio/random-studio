29ca68c5614b35a7ef9bd49b8910e80e583e9c96
Andrew Hill <andrew.hill@random.studio>
2019-11-25 16:14:41 +0100

    Adjust image-remark settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,93 +1,100 @@
-const postCssCustomProperties = require("postcss-custom-properties")
+const postCssCustomProperties = require('postcss-custom-properties');
 
 const gatsbyConfig = {
   plugins: [
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: 'gatsby-plugin-sass',
       options: {
         postCssPlugins: [postCssCustomProperties()],
       },
     },
     {
-      resolve: `gatsby-plugin-polyfill-io`, //  ios12> & ie11
+      resolve: 'gatsby-plugin-polyfill-io', //  ios12> & ie11
       options: {
-        features: ["IntersectionObserver"],
+        features: ['IntersectionObserver'],
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
-        name: "uploads",
+        name: 'uploads',
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
+        name: 'pages',
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/site`,
-        name: "site",
+        name: 'site',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-relative-images",
+            resolve: 'gatsby-remark-relative-images',
             // options: {
             //   name: "uploads",
             // },
           },
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 2048,
+              quality: 90,
+              tracedSVG: {
+                color: '#eeeeee',
+              },
+              withWebp: {
+                quality: 90,
+              },
             },
           },
           {
-            resolve: "gatsby-remark-copy-linked-files",
+            resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: "static",
+              destinationDir: 'static',
             },
           },
         ],
       },
     },
     {
-      resolve: "gatsby-plugin-netlify-cms",
+      resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
 
 // CONTEXT is a netlify env
-if (process.env.CONTEXT === "production") {
+if (process.env.CONTEXT === 'production') {
   const gaConfig = {
-    resolve: `gatsby-plugin-google-analytics`,
+    resolve: 'gatsby-plugin-google-analytics',
     options: {
-      trackingId: "UA-9384788-13",
+      trackingId: 'UA-9384788-13',
       head: true,
       anonymize: true,
       respectDNT: true,
     },
-  }
+  };
 
-  gatsbyConfig.plugins.push(gaConfig)
+  gatsbyConfig.plugins.push(gaConfig);
 }
 
-module.exports = gatsbyConfig
+module.exports = gatsbyConfig;
