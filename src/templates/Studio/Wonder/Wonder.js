db47ad8058981c2ecf4d3b45caefb439a3c577d6
Andrew Hill <andrew.hill@random.studio>
2019-11-29 15:11:31 +0100

    Rewrite Wonder

--- src/templates/Studio/Wonder/Wonder.js
+++ src/templates/Studio/Wonder/Wonder.js
@@ -1,123 +1,178 @@
 import './Wonder.scss';
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import {
-  UniversalCamera,
+  Animation,
+  HemisphericLight,
+  StandardMaterial,
+  MirrorTexture,
   Engine,
-  PointLight,
+  MeshBuilder,
   Scene,
   Vector3,
   SceneLoader,
   Color3,
   ArcRotateCamera,
+  Plane,
 } from 'babylonjs';
 import 'babylonjs-loaders';
 
 
 const Wonder = () => {
   const canvasRef = useRef();
-  const rotationRef = useRef([0, 0]);
-  const windowSizeRef = useRef([window.innerWidth, window.innerHeight]);
 
-  const cameraRef = useRef();
-  const [cameraPos, setCameraPos] = useState({
-    x: -1,
-    y: 5,
-    radius: -6,
-    tX: -2,
-    tY: 2,
-    tZ: 0,
-  });
-  const to = {
-    x: -3.5,
-    y: 5,
-    radius: -5.5,
-    tX: -0.15,
-    tY: 2,
-    tZ: 0,
-  };
-  const setCameraXPos = (value) => setCameraPos(c => ({...c, x: parseFloat(value)}))
-  const setCameraYPos = (value) => setCameraPos(c => ({...c, y: parseFloat(value)}))
-  const setCameraRPos = (value) => setCameraPos(c => ({...c, radius: parseFloat(value)}))
-  const setCameraTXPos = (value) => setCameraPos(c => ({...c, tX: parseFloat(value)}))
-  const setCameraTYPos = (value) => setCameraPos(c => ({...c, tY: parseFloat(value)}))
-  const setCameraTZPos = (value) => setCameraPos(c => ({...c, tZ: parseFloat(value)}))
+  const cameraRotationVectors = [
+    {
+      frame: 0,
+      value: new Vector3(-1, 5, -5),
+    },
+    {
+      frame: 1000,
+      value: new Vector3(-3.5, 5, -5.5),
+    },
+    {
+      frame: 2000,
+      value: new Vector3(-1, 5, -6),
+    },
+  ];
+
+  const cameraTargetVectors = [
+    {
+      frame: 0,
+      value: new Vector3(-2, 2, 0),
+    },
+    {
+      frame: 1000,
+      value: new Vector3(-0.15, 2, 0),
+    },
+    {
+      frame: 2000,
+      value: new Vector3(-2, 2, 0),
+    },
+  ];
+
+  const mirrors = [
+    {
+      width: 1.4,
+      height: 2.3,
+      position: new Vector3(-0.92, 1.58, 3.65),
+      rotation: new Vector3(0, -0.31, 0),
+    },
+    {
+      width: 0.45,
+      height: 2.3,
+      position: new Vector3(-1.654, 1.58, 3.65),
+      rotation: new Vector3(0, 89.23, 0),
+    },
+  ];
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const engine = new Engine(canvasRef.current, true, {
+    let engine;
+
+    const createEngine = () => {
+      engine = new Engine(canvasRef.current, true, {
         preserveDrawingBuffer: true,
         stencil: true,
       });
+      engine.doNotHandleContextLost = true;
+    };
 
+    const createScene = () => {
       const scene = new Scene(engine);
-      scene.createDefaultLight();
+      scene.clearColor = new Color3(0.972549, 0.972549, 0.972549);
+      return scene;
+    };
 
-      let { x, y, radius, tX, tY, tZ } = cameraPos;
-      const camera = new ArcRotateCamera('Camera', x, y, radius, new Vector3(tX, tY, tZ), scene, true);
+    const addCamera = scene => {
+      const camera = new ArcRotateCamera('camera', 0, 0, 0, cameraTargetVectors[0].value, scene, true);
+      camera.setPosition(cameraRotationVectors[0].value);
+      camera.setTarget(cameraTargetVectors[0].value);
       camera.attachControl(canvasRef.current, true);
       camera.minZ = 0;
-      cameraRef.current = camera
-      scene.clearColor = new Color3(0.972549, 0.972549, 0.972549);
-      SceneLoader.ShowLoadingScreen = false;
-      SceneLoader.Append('/models/', 'test-island.obj', scene);
+      return camera;
+    };
 
-      const cameraMap = Object.values(cameraPos);
-      const steps = Object.keys(cameraPos).map(x => (to[x] - cameraPos[x]) / 2000);
-      let step = 0;
-      let direction = 1;
+    const addLighting = scene => {
+      const light = new HemisphericLight('hemi', new Vector3(0, 10, -5), scene);
+      light.specular = new Color3(0, 0, 0);
+      light.specularPower = 0;
+      light.intensity = 2;
+      return light;
+    };
+
+    const addModel = async scene => {
+      SceneLoader.ShowLoadingScreen = false;
+      await SceneLoader.AppendAsync('/models/', 'test-island.obj', scene);
+    };
+
+    const addMirrors = scene => {
+      mirrors.forEach(mirrorState => {
+        const mirror = MeshBuilder.CreatePlane('glass', {
+          width: mirrorState.width,
+          height: mirrorState.height,
+        }, scene);
+        mirror.position = mirrorState.position;
+        mirror.rotation = mirrorState.rotation;
+        mirror.computeWorldMatrix(true);
+        const mirrorWorldMatrix = mirror.getWorldMatrix();
+        const mirrorVertexData = mirror.getVerticesData('normal');
+        let mirrorNormal = new Vector3(mirrorVertexData[0], mirrorVertexData[1], mirrorVertexData[2]);
+        mirrorNormal = new Vector3.TransformNormal(mirrorNormal, mirrorWorldMatrix);
+
+        const reflector = new Plane.FromPositionAndNormal(mirror.position, mirrorNormal.scale(-1));
+        const mirrorMaterial = new StandardMaterial('MirrorMat', scene);
+        mirrorMaterial.diffuseColor = new Color3(0.1, 0.1, 0.1);
+        mirrorMaterial.reflectionTexture = new MirrorTexture('mirror', 1024, scene, true);
+        mirrorMaterial.reflectionTexture.mirrorPlane = reflector;
+        mirrorMaterial.reflectionTexture.renderList = [scene.meshes.find(mesh => mesh.id === 'Default')];
+        mirror.material = mirrorMaterial;
+      });
+    };
+
+    const setupRenderLoop = scene => {
       engine.runRenderLoop(() => {
-        const newValue = i => cameraMap[i] + (steps[i] * step);
-        cameraRef.current.setPosition(new Vector3(newValue(0), newValue(1), newValue(2)));
-        cameraRef.current.setTarget(new Vector3(newValue(3), newValue(4), newValue(5)));
-        step += direction;
-        if (step > 2000 || step < -2000) {
-          direction *= -1;
-        }
+        // We don't use physics etc so can safely clear this
+        scene.clearCachedVertexData();
+        scene.cleanCachedTextureBuffer();
         scene.render();
       });
+    };
 
-      window.addEventListener('resize', () => {
-        engine.resize();
-        windowSizeRef.current = [window.innerWidth, window.innerHeight];
-      });
+    const setupCameraAnimation = (scene, camera) => {
+      const targetAnim = new Animation('targetAnim', 'target', 60, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CYCLE);
+      const rotAnim = new Animation('rotationAnim', 'rotation', 60, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CYCLE);
+      targetAnim.setKeys(cameraTargetVectors);
+      rotAnim.setKeys(cameraRotationVectors);
+      camera.animations.push(rotAnim);
+      camera.animations.push(targetAnim);
+      scene.beginAnimation(camera, 0, 2000, true);
+    };
 
-      window.addEventListener('mousemove', e => {
-        const [width, height] = windowSizeRef.current;
-        const hDirection = e.clientX > width / 2 ? 1 : -1;
-        const vDirection = e.clientY > height / 2 ? 1 : -1;
-        rotationRef.current = [hDirection, vDirection];
-      });
+    const onResize = () => engine.resize();
+
+    const createGLScene = async () => {
+      createEngine();
+      const scene = createScene();
+      const camera = addCamera(scene);
+      const light = addLighting(scene);
+      await addModel(scene);
+      addMirrors(scene);
+      setupRenderLoop(scene, camera);
+      setupCameraAnimation(scene, camera);
+
+      window.addEventListener('resize', onResize);
+    };
+
+    if (canvasRef.current) {
+      createGLScene();
     }
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+      engine.dispose();
+    };
   }, [canvasRef]);
 
-  useEffect(() => {
-    console.log(cameraPos, cameraRef.current)
-    if (cameraRef.current) {
-      console.log('set post')
-      cameraRef.current.setPosition(new Vector3(cameraPos.x, cameraPos.y, cameraPos.radius));
-      cameraRef.current.setTarget(new Vector3(cameraPos.tX, cameraPos.tY, cameraPos.tZ));
-    }
-  }, [cameraPos]);
-
-  return (
-    <>
-      <canvas ref={canvasRef} className="canvas" />
-      <div className="admin">
-        <div>
-          Rotate
-          <input type="number" step="0.01" value={cameraPos.x} onChange={e => setCameraXPos(e.target.value)} />
-          <input type="number" step="0.01" value={cameraPos.y} onChange={e => setCameraYPos(e.target.value)} />
-          <input type="number" step="0.01" value={cameraPos.radius} onChange={e => setCameraRPos(e.target.value)} />
-        </div>
-        <div>
-          Target
-          <input type="number" step="0.01" value={cameraPos.tX} onChange={e => setCameraTXPos(e.target.value)} />
-          <input type="number" step="0.01" value={cameraPos.tY} onChange={e => setCameraTYPos(e.target.value)} />
-          <input type="number" step="0.01" value={cameraPos.tZ} onChange={e => setCameraTZPos(e.target.value)} />
-        </div>
-      </div>
-    </>
-  );
+  return <canvas ref={canvasRef} className="canvas" />;
 };
 
 export default Wonder;
