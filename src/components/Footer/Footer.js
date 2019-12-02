caa45e5b72423fb3d6742c26295bc1a641f2f1ac
Andrew Hill <andrew.hill@random.studio>
2019-12-02 17:08:42 +0100

    Break out phone and email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import styles from './Footer.module.scss';
 
-export default ({ address, contact }) => (
-  <footer className={styles.footer}>
-    <div>
-      <ReactMarkdown escapeHtml={false} source={address} linkTarget="__blank" />
-    </div>
+export default ({ address, contact }) => {
+  const desktopBreakpoint = 1152;
 
-    <div>
-      <ReactMarkdown escapeHtml={false} source={contact} />
-    </div>
-  </footer>
-);
+  const email = 'hello@random.studio';
+  const phone = '+31 20 779 7735';
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <ReactMarkdown escapeHtml={false} source={address} linkTarget="__blank" />
+      </div>
+
+      <div>
+        <a href={`tel:${phone.replace(' ', '-')}`}>{phone}</a>
+        <ReactMarkdown escapeHtml={false} source={email} />
+      </div>
+    </footer>
+  );
+};
