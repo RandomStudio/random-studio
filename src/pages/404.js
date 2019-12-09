0f522376e68231b0b3fd08dfd3b78b88de968265
Andrew Hill <andrew.hill@random.studio>
2019-12-09 17:58:16 +0100

    Restore 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './404.module.scss';
-import Layout from '../../components/Layout/Layout';
-import Navigation from '../../components/Navigation/Navigation';
+import Layout from '../components/Layout/Layout';
+import Navigation from '../components/Navigation/Navigation';
 
 const Error404 = () => (
   <Layout>
