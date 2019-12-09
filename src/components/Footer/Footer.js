b128d74c45560c54d45e4d644e8b6d09493b5d09
Andrew Hill <andrew.hill@random.studio>
2019-12-09 14:07:43 +0100

    Never show two email address links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import styles from './Footer.module.scss';
 export default ({ address, email, phone }) => {
   const [isNoticeVisible, setIsNoticeVisible] = useState(false);
   const emailRef = useRef();
+
   const handleClickEmail = e => {
     try {
       window.getSelection().selectAllChildren(emailRef.current);
