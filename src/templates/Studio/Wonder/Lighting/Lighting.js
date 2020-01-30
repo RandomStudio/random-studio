9c5ce6400d9fada668bdd282c98e12389fb3ecf0
Jurgis <jurgis.lietunovas@random.studio>
2020-01-30 17:53:09 +0100

    add hdr, fix eslint complaint

--- src/templates/Studio/Wonder/Lighting/Lighting.js
+++ src/templates/Studio/Wonder/Lighting/Lighting.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { HemisphericLight, Vector3, Color3 } from 'babylonjs';
+import { useEffect } from 'react';
+import { HemisphericLight, CubeTexture, Vector3, Color3 } from 'babylonjs';
 
-const Lighting = ({ scene }) => {
+const Lighting = ({ scene, hdr }) => {
   useEffect(() => {
     if (!scene) {
       return undefined;
@@ -10,16 +10,24 @@ const Lighting = ({ scene }) => {
     const generalLight = new HemisphericLight(
       'hemi',
       new Vector3(0, 10, -5),
-      scene
+      scene,
     );
     generalLight.specular = new Color3(0, 0, 0);
     generalLight.specularPower = 0;
     generalLight.intensity = 0.4;
 
+    const hdrTexture = new CubeTexture.CreateFromPrefilteredData(
+      hdr.url,
+      scene,
+    );
+
+    hdrTexture.gammaSpace = false;
+    scene.environmentTexture = hdrTexture;
+
     return () => {
       generalLight.dispose();
     };
-  }, [scene]);
+  }, [hdr.url, scene]);
 
   return null;
 };
