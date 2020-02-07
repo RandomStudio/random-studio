047f5a73ea22fd288be334160065b3588ad5eb4b
Jurgis <jurgis.lietunovas@random.studio>
2020-02-07 16:15:16 +0100

    remove console.log

--- src/utils/checkAndroid.js
+++ src/utils/checkAndroid.js
@@ -1,9 +1,7 @@
 const checkAndroid = () => {
   if (navigator.userAgent.match(/Android|webOS/)) {
-    console.log('hello android');
     return true;
   } else {
-    console.log('not android');
     return false;
   }
 };
