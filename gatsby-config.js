d3dadfca7783244a44a31a33ecbb44c41b5029c3
Jurgis <jurgis.lietunovas@random.studio>
2020-02-07 11:50:44 +0100

    add analytics for deploy-preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ const gatsbyConfig = {
 };
 
 // CONTEXT is a netlify env
-if (process.env.CONTEXT === 'production') {
+if (process.env.CONTEXT === 'production' || process.env.CONTEXT === 'deploy-preview') {
   const gaConfig = {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
