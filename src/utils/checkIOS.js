ddca665e741085caefe11927095e945576259e28
Kangyun Wang <me@kyunwang.nl>
2020-04-15 11:09:03 +0200

    Add checks for window

--- src/utils/checkIOS.js
+++ src/utils/checkIOS.js
@@ -1,6 +1,6 @@
 // Source: https://stackoverflow.com/a/58064481
-const checkIOS = () => (/iPad|iPhone|iPod/.test(navigator.platform) ||
+const checkIOS = () => typeof window !== `undefined` && ((/iPad|iPhone|iPod/.test(navigator.platform) ||
 (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
-!window.MSStream;
+!window.MSStream);
 
 export default checkIOS;
