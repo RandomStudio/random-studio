4a0438cec3990f3ef2f6e843a44b1e5fa6c566ce
Jurgis <jurgis.lietunovas@random.studio>
2020-04-16 18:00:58 +0200

    WIP image carousel

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -5,6 +5,7 @@ import styles from './ImageCarousel.module.scss';
 import FluidImage from '../FluidImage/FluidImage';
 
 const ImageCarousel = ({
+  media,
   images,
   showIndicator,
   title,
@@ -13,23 +14,71 @@ const ImageCarousel = ({
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log('images in carousel:', images);
+  console.log('media:', media);
 
-  const handleNextImage = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
+  const handleNextImage = (imgs) => {
+    setCurrentIndex((currentIndex + 1) % imgs.length);
   };
 
-  if (!images) return null;
+  if (media) {
+    return (
+      <div className={`${styles.carousel} ${className}`}>
+        {media.map(
+          (
+            { marginTop, marginLeft, width, carouselImages, caption },
+            index,
+          ) => (
+            <div
+              className={`
+          ${styles.image}
+              ${index === currentIndex && styles.imageVisible}
+            `}
+              style={
+                ({
+                  '--marginTop': `${marginTop}%`,
+                  '--marginLeft': `${marginLeft}%`,
+                  '--width': `${width}%`,
+                },
+                index === currentIndex
+                  ? { display: 'block', opacity: 1 }
+                  : { display: 'none', opacity: 0 })
+              }
+            >
+              <div
+                className={`${carouselImages.length > 1 && styles.hasMultiple}`}
+                onClick={() => handleNextImage(carouselImages)}
+              >
+                {carouselImages.map(({ image }) => {
+                  {
+                    image.childImageSharp ? (
+                      <FluidImage
+                        image={image}
+                        objectFit={objectFit}
+                        loading="auto"
+                      />
+                    ) : (
+                      <img alt="" src={image} />
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          ),
+        )}
+      </div>
+    );
+  }
 
+  if (!images) return null;
   return (
     <div className={`${styles.carousel} ${className}`}>
-      {images.map(({ image, caption }, index) => (
+      {(images || []).map(({ image, caption }, index) => (
         <div
           className={`
-            ${styles.image}
+        ${styles.image}
             ${index === currentIndex && styles.imageVisible}
           `}
-          key={image.childImageSharp.fluid.src}
+          // key={image.childImageSharp.fluid.src}
           style={
             index === currentIndex
               ? { display: 'block', opacity: 1 }
@@ -40,7 +89,11 @@ const ImageCarousel = ({
             className={`${images.length > 1 && styles.hasMultiple}`}
             onClick={handleNextImage}
           >
-            <FluidImage image={image} objectFit={objectFit} loading="auto" />
+            {image.childImageSharp ? (
+              <FluidImage image={image} objectFit={objectFit} loading="auto" />
+            ) : (
+              <img alt="" src={image} />
+            )}
           </div>
           {caption && <ReactMarkdown escapeHtml={false} source={caption} />}
         </div>
