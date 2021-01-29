7062e6fcf6d6cedb365deef1280ab49af9e56298
Larix Kortbeek <larixk@gmail.com>
2021-01-29 13:33:20 +0100

    Add Random Paris in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,14 +16,13 @@ const Footer = ({ address, email, phone }) => {
     <footer className={styles.footer}>
       <div className={styles.studio}>
         <div className={`${styles.address} ${styles.column}`}>
+          <span className={styles.name}>Random Amsterdam</span>
           <ReactMarkdown
+            className={styles.location}
             escapeHtml={false}
             source={address}
             linkTarget="__blank"
           />
-        </div>
-
-        <div className={styles.column}>
           <a className={styles.phone} href={`tel:${phone.replace(' ', '-')}`}>
             {phone}
           </a>
@@ -37,6 +36,34 @@ const Footer = ({ address, email, phone }) => {
           </a>
           <a className={styles.emailMobile} href={`mailto:${email}`}>
             {email}
+          </a>
+        </div>
+        </div>
+      <div className={styles.studio}>
+        <div className={`${styles.address} ${styles.column}`}>
+          <span className={styles.name}>Random Paris</span>
+          <div
+            className={styles.location}
+          >
+            174 Quai de Jemmapes<br />
+            75010 Paris<br />
+            France<br />
+            <a href="https://goo.gl/maps/8JhU7KsTLAaZZtGA9" target="_blank">Directions</a>
+          </div>
+
+          <a className={styles.phone} href={`tel:+33-(0)-1-40-36-41-44`}>
+            {"+33 (0) 1 40 36 41 44"}
+          </a>
+          <a
+            className={styles.emailDesktop}
+            href={`mailto:morgan.maccari@random.studio`}
+            onClick={handleClickEmail}
+            ref={emailRef}
+          >
+            {"morgan.maccari@random.studio"}
+          </a>
+          <a className={styles.emailMobile} href={`mailto:morgan.maccari@random.studio`}>
+            {"morgan.maccari@random.studio"}
           </a>
         </div>
       </div>
