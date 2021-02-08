e9495106c05e67439ba4cc6e8ebe1b4774258ffe
Kangyun Wang <me@kyunwang.nl>
2021-02-08 09:27:48 +0100

    Add missing autostart past live

--- src/random-paris/components/TakeOverHeader/TakeOverHeader.js
+++ src/random-paris/components/TakeOverHeader/TakeOverHeader.js
@@ -7,18 +7,20 @@ import useTimeFromStart from '../../hooks/useTimeFromStart';
 import gpsData from '../../utils/gpsWithData.json';
 
 const TakeOverHeader = () => {
-	const timeAfterStart = useTimeFromStart();
+  const timeAfterStart = useTimeFromStart();
 
-	// Total duration in seconds of the whole journey
-	const totalDuration = useMemo(() => gpsData.length * 60, []);
+  // Total duration in seconds of the whole journey
+  const totalDuration = useMemo(() => gpsData.length * 60, []);
 
-	if (timeAfterStart === null) {
-		return null;
-	}
+  if (timeAfterStart === null) {
+    return null;
+  }
 
-	const isBeforeLive = timeAfterStart < 0;
-	const isDuringLive = timeAfterStart >= 0 && timeAfterStart < totalDuration * 1000;
-	return <Main isBeforeLive={isBeforeLive} isLive={isDuringLive} totalDuration={totalDuration} />;
+  const isBeforeLive = timeAfterStart < 0;
+  const isDuringLive = timeAfterStart >= 0 && timeAfterStart < totalDuration * 1000;
+  const isPastLive = timeAfterStart > totalDuration * 1000;
+
+  return <Main isBeforeLive={isBeforeLive} isLive={isDuringLive} isPastLive={isPastLive} totalDuration={totalDuration} />;
 };
 
 TakeOverHeader.propTypes = {};
