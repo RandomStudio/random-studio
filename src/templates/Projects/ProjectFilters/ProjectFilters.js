54249495d41c83cbee18c30f0233b4f6c1b74e3e
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2021-07-13 15:58:24 +0200

    add filters

--- src/templates/Projects/ProjectFilters/ProjectFilters.js
+++ src/templates/Projects/ProjectFilters/ProjectFilters.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styles from './ProjectFilters.module.scss';
 
 const FILTERS = [
   'Spatial Experience',
@@ -11,9 +12,29 @@ const FILTERS = [
 ];
 
 const ProjectFilters = ({ projects }) => {
-  console.log(projects);
+  const [activeTag, setActiveTag] = useState(null);
 
-  return <div>{'filter'}</div>;
+  useEffect(() => {
+    console.log(activeTag);
+  }, [activeTag]);
+
+  return (
+    <div className={styles.wrapper}>
+      {FILTERS.map((filter) => (
+        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+        <div
+          className={`${styles.filter} ${
+            filter === activeTag && styles.active
+          }`}
+          onClick={() => setActiveTag(filter === activeTag ? null : filter)}
+          key={filter}
+        >
+          {filter}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProjectFilters;
