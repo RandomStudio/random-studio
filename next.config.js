4dc5d9b94adb935f1692fca511a1811d1e0ad913
Andrew Hill <andrew.hill@random.studio>
2021-12-03 18:43:41 +0100

    Use env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  env: {
-    CDN_URL: process.env.NODE_ENV === 'production'
-      ? 'https://d3j85u6uj65sfx.cloudfront.net'
-      : '',
-  },
   experimental: { esmExternals: true },
 };
