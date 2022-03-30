da106db52104ca30b8715dde894ae5797253f002
Andrew Hill <contact@andrewthomashill.co.uk>
2022-03-30 12:55:52 +0200

    Simplify env

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.REVALIDATE_SECURITY_TOKEN) {
+  if (req.query.secret !== process.env.GITHUB_OAUTH_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
