9d9fc04d7a6fb8701d21bdd365233f127d79218c
Andrew Hill <contact@andrewthomashill.co.uk>
2022-04-07 16:11:36 +0200

    Move redirects to JS

--- next.config.js
+++ next.config.js
@@ -10,13 +10,34 @@ module.exports = withBundleAnalyzer({
   async redirects() {
     return [
       {
-        source: '/NikeLab-ACG',
-        destination: '/nikelab-acg',
+        source: '/admin',
+        destination: 'https://random-studio.admin.datocms.com',
         permanent: false,
       },
       {
-        source: '/NikeLab-Tech-Pack',
-        destination: '/nikelab-tech-pack',
+        source: '/newsletter',
+        destination: 'http://eepurl.com/gebngv',
+        permanent: false,
+      },
+      {
+        source: '/chloe',
+        destination:
+          'https://random.studio/projects/a-ss21-collection-experience-for-chloe',
+        permanent: false,
+      },
+      {
+        source: '/nicetomeetyou',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/projects/NikeLab-ACG',
+        destination: '/projects/nikelab-acg',
+        permanent: false,
+      },
+      {
+        source: '/projects/NikeLab-Tech-Pack',
+        destination: '/projects/nikelab-tech-pack',
         permanent: false,
       },
     ];
