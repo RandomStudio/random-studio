d381f09222eee5ba236ccef961f3ede151a838c5
jondujaka <jdujaka@gmail.com>
2023-02-17 16:33:15 +0100

    Add revalidate api

---
+++ src/pages/api/revalidate.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.DATOCMS_PREVIEW_SECRET) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  const slug = req.body?.entity?.collaboration_url?.replace(
+    'https://random.studio',
+    '',
+  );
+
+  if (!slug) {
+    return res.status(401).json({ message: 'You need to pass a slug' });
+  }
+
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate(slug);
+
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
