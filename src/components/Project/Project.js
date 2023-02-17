4ac4fd0877e20c7067a0b2291e2aef18d36f0969
jondujaka <jdujaka@gmail.com>
2023-02-17 16:32:12 +0100

    update packages

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -17,49 +17,49 @@ const Project = ({
   slug,
   width,
 }) => (
-  <Link href={`/projects/${slug}`} id={slug}>
-    <a
-      className={`${styles.thumbnail} ${className}`}
-      style={{
-        marginLeft: `${left}%`,
-        marginTop: `${top}%`,
-        width: `${width}%`,
-      }}
-    >
-      <div className={styles.media}>
-        {featuredVideo ? (
-          <VideoWithControls
-            autoplay
-            hasAudio={false}
-            hasControls={false}
-            loops
-            video={featuredVideo}
-            width={width}
-          />
-        ) : (
-          <Image
-            alt="" // Keeps the screen reader focused on project list
-            data={featuredImage.imageData}
-            sizes={`(max-width: 576px) 100vw, ${width}vw`}
-          />
-        )}
-      </div>
+  <Link
+    className={`${styles.thumbnail} ${className}`}
+    href={`/projects/${slug}`}
+    id={slug}
+    style={{
+      marginLeft: `${left}%`,
+      marginTop: `${top}%`,
+      width: `${width}%`,
+    }}
+  >
+    <div className={styles.media}>
+      {featuredVideo ? (
+        <VideoWithControls
+          autoplay
+          hasAudio={false}
+          hasControls={false}
+          loops
+          video={featuredVideo}
+          width={width}
+        />
+      ) : (
+        <Image
+          alt="" // Keeps the screen reader focused on project list
+          data={featuredImage.imageData}
+          sizes={`(max-width: 576px) 100vw, ${width}vw`}
+        />
+      )}
+    </div>
 
-      <div
-        className={styles.title}
-        style={
-          !left || left < 1
-            ? {}
-            : {
-              marginLeft: '0',
-            }
-        }
-      >
-        <ReactMarkdown>
-          {title.replace('<br />', '<br>').replace('<br>', '\n\n')}
-        </ReactMarkdown>
-      </div>
-    </a>
+    <div
+      className={styles.title}
+      style={
+        !left || left < 1
+          ? {}
+          : {
+            marginLeft: '0',
+          }
+      }
+    >
+      <ReactMarkdown>
+        {title.replace('<br />', '<br>').replace('<br>', '\n\n')}
+      </ReactMarkdown>
+    </div>
   </Link>
 );
 
