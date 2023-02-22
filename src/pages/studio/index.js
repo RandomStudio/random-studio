2de5d70937de2a3ed77d9f2dbdd47e4ee59cad38
jondujaka <jdujaka@gmail.com>
2023-02-22 15:40:16 +0100

    clear studio page just to test

--- src/pages/studio/index.js
+++ src/pages/studio/index.js
@@ -23,103 +23,11 @@ const socialLinks = {
 const Studio = ({ blurb, blocks, skillset, studioImpression, vacancies }) => (
   <Layout isNewDesign>
     <Head />
-
-    <p className={styles.blurb} dangerouslySetInnerHTML={{ __html: blurb }} />
-
-    {skillset && (
-      <div className={styles.skills}>
-        <ul className={styles.list}>
-          <li className={styles.heading}>{'Design'}</li>
-
-          {skillset.design?.map(designArea => (
-            <li key={designArea}>{designArea}</li>
-          ))}
-        </ul>
-
-        <ul className={styles.list}>
-          <li className={styles.heading}>{'Technology'}</li>
-
-          {skillset.technology?.map(technologyArea => (
-            <li key={technologyArea}>{technologyArea}</li>
-          ))}
-        </ul>
-
-        <ul className={styles.list}>
-          <li className={styles.heading}>{'Product'}</li>
-
-          {skillset.product?.map(productArea => (
-            <li key={productArea}>{productArea}</li>
-          ))}
-        </ul>
-      </div>
-    )}
-
-    <Carousel
-      caption="Studio Impressions"
-      className={styles.carousel}
-      sizes="(max-width: 768px) 100vw, 50vw"
-      slides={studioImpression}
-    />
-
-    {blocks.map(block => (
-      <Block
-        block={block}
-        className={`${styles.block} ${styles.bcorp}`}
-        key={block.copy}
-      />
-    ))}
-
-    <div className={styles.links}>
-      <div className={styles.list}>
-        <h3 className={styles.heading}>{'Start a conversation'}</h3>
-
-        <a href="mailto:hello@random.studio">{'hello@random.studio'}</a>
-      </div>
-
-      <div className={styles.list}>
-        <h3 className={styles.heading}>{'New business inquiries'}</h3>
-
-        <a href="mailto:business@random.studio">{'business@random.studio'}</a>
-      </div>
-
-      <div className={styles.list}>
-        <h3 className={styles.heading}>{'Press inquiries'}</h3>
-
-        <a href="mailto:press@random.studio">{'press@random.studio'}</a>
-      </div>
-
-      <div className={styles.list}>
-        <h3 className={styles.heading}>{'Follow us'}</h3>
-
-        <div className={styles.social}>
-          {Object.entries(socialLinks).map(([name, url]) => (
-            <div className={styles.link} key={name}>
-              <a href={url} rel="noopener noreferrer" target="_blank">
-                {name}
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <Vacancies className={styles.vacancies} vacancies={vacancies} />
-    </div>
+    Test
   </Layout>
 );
 
-export const getStaticProps = async preview => {
-  const { page } = await getDataFromBackend({
-    query: STUDIO_PAGE_QUERY,
-    preview,
-  });
-
-  return {
-    props: {
-      ...page,
-      blocks: await addAdditionalInfoToBlocks(page.blocks),
-    },
-  };
-};
+export const getStaticProps = async preview => { };
 
 Studio.propTypes = {
   blocks: PropTypes.arrayOf(dayNightImageBlockPropType).isRequired,
