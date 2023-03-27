b672dfe1fd94953c3472dfbc5526d7678aa50963
Andrew Hill <contact@andrewthomashill.co.uk>
2023-03-27 10:46:28 +0200

    TS ignore migration script

--- cms/tools/migrate-vimeo.ts
+++ cms/tools/migrate-vimeo.ts
@@ -1,3 +1,4 @@
+//@ts-ignore
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { buildClient, buildBlockRecord, Client } from '@datocms/cma-client-node';
 dotenv.config()
