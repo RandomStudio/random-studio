7bb494fa40d56d792f4adf46d97ed66192eeeaac
jondujaka <jdujaka@gmail.com>
2023-06-02 12:57:55 +0200

    enable previews on all environment except production

--- src/api/getDataFromBackend.ts
+++ src/api/getDataFromBackend.ts
@@ -1,12 +1,22 @@
-import { GraphQLClient, RequestDocument } from 'graphql-request';
+import { GraphQLClient, RequestDocument } from "graphql-request";
 
-const getDataFromBackend = async ({ query, variables, preview }: { query: RequestDocument, variables?: unknown, preview?: boolean }) => {
+const getDataFromBackend = async ({
+  query,
+  variables,
+  preview,
+}: {
+  query: RequestDocument;
+  variables?: unknown;
+  preview?: boolean;
+}) => {
   const endpoint = `https://graphql.datocms.com/`;
+
+  const includeDrafts = preview ?? process.env.CONTEXT !== "production";
 
   const client = new GraphQLClient(endpoint, {
     headers: {
       authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
-      ...(preview ? { 'X-Include-Drafts': 'true' } : {}),
+      ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
     },
   });
 
