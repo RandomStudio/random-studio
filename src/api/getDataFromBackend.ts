acfe99384765ab44b51ab1dfed134cc6d4595703
Andrew Hill <contact@andrewthomashill.co.uk>
2023-07-05 15:23:21 +0200

    Match env variable used on Netlify

--- src/api/getDataFromBackend.ts
+++ src/api/getDataFromBackend.ts
@@ -3,7 +3,7 @@ import { GraphQLClient, RequestDocument } from 'graphql-request';
 const getDataFromBackend = async ({ query, variables, preview, }: { query: RequestDocument, variables?: unknown, preview?: boolean, }) => {
   const endpoint = 'https://graphql.datocms.com/';
 
-  const includeDrafts = preview ?? process.env.CONTEXT !== 'production';
+  const includeDrafts = preview ?? process.env.IGNORE_DRAFT_DATA !== 'true';
 
   const client = new GraphQLClient(endpoint, {
     headers: {
