21667897344c640010838102d04613d30d37fa35
jondujaka <jdujaka@gmail.com>
2023-12-04 13:29:00 +0100

    Add sample

--- src/components/Studio/Block/Block.tsx
+++ src/components/Studio/Block/Block.tsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
-import Image from '../../Image/Image';
+
 import AfterDarkContext from '../../Layout/AfterDarkContext';
 import styles from './Block.module.css';
 import Markdown from '../../Markdown/Markdown';
 import { DayNightImageBlock } from '../../../types/types';
+import PixelatedImage from '../../PixelatedImage/PixelatedImage';
 
 type BlockProps = {
   block: DayNightImageBlock;
@@ -19,7 +20,7 @@ const Block = ({
   return (
     <div className={`${styles.block} ${className}`}>
       <div className={`${styles.image} image`}>
-        <Image alt={copy} data={(isAfterDark ? nightImage : image).imageData} />
+        <PixelatedImage image={(isAfterDark ? nightImage : image).imageData} />
       </div>
 
       <Markdown className={styles.text} markdown={copy} />
