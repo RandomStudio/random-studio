514a24023a717ade9cc4910e027357e8f3745107
Andrew Hill <contact@andrewthomashill.co.uk>
2024-01-08 15:30:55 +0100

    Add handler

--- netlify/functions/getPreviewUrls.ts
+++ netlify/functions/getPreviewUrls.ts
@@ -1,5 +1,6 @@
 import type { HandlerEvent } from '@netlify/functions';
 
+// This function is used to generate preview URLs for the side-by-side plugin in DatoCMS
 const handler = async (event: HandlerEvent) => {
   const params = new URLSearchParams(event.rawQuery);
   const slug = params.get('slug');
