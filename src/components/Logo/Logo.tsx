3ecf80a27a46be039597c934a75696297fd76087
jondujaka <jdujaka@gmail.com>
2024-01-08 17:36:16 +0100

    temporary 2024 greeting

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
+import classNames from 'classnames';
 import styles from './Logo.module.css';
 
 type LogoProps = {
@@ -7,17 +8,55 @@ type LogoProps = {
 };
 
 const Logo = ({ isCentred }: LogoProps) => {
-  const centredClass = isCentred ? styles.isCentre : styles.isTop;
-  const logoClass = `${styles.logo} ${centredClass}`;
+  const [hasGreeting, setHasGreeting] = useState(true);
+
+  const logoClass = classNames(styles.logo, {
+    [styles.hasGreeting]: hasGreeting,
+    [styles.isCentre]: isCentred,
+    [styles.isTop]: !isCentred,
+  });
+
+  const handleScroll = useCallback(() => {
+    if (!hasGreeting) {
+      return;
+    }
+
+    const currentScroll = window.scrollY;
+
+    const windowHeight = window.innerHeight;
+    const calculatedHeight = window.innerWidth / 1.777;
+
+    const threshold = Math.min(windowHeight, calculatedHeight);
+
+    if (currentScroll > threshold) {
+      setHasGreeting(false);
+    }
+  }, [hasGreeting]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
+  const textTop = useMemo(
+    () => (hasGreeting ? 'Happy' : 'Random'),
+    [hasGreeting],
+  );
+
+  const textBottom = useMemo(
+    () => (hasGreeting ? '2024' : 'Studio'),
+    [hasGreeting],
+  );
 
   return (
     <h1 className={logoClass}>
       <Link href="/">
-        {'Random '}
+        {textTop}
 
         <br aria-hidden />
 
-        {'Studio'}
+        {textBottom}
       </Link>
     </h1>
   );
