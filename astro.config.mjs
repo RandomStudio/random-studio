fcead571f19b7814c14a686a54c3453661e85d03
Andrew Hill <contact@andrewthomashill.co.uk>
2025-12-22 13:19:28 +0000

    Add netlify plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact({
     compat: true
-  })]
+  })],
+
+  adapter: netlify()
 });
